7420df3fe76446b4acdb4d094448a62e298adf4c
frodmori <franciscodepaula.rodriguezmorillo.st@everis.nttdata.com>
2022-03-30 08:42:23 +0200

    initial commit

---
+++ src/main/java/com/everis/nttdatacenters_jdbc_t1_frm/App.java
@@ -0,0 +1,13 @@
+package com.everis.nttdatacenters_jdbc_t1_frm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
